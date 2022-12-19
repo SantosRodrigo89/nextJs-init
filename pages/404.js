00d61db37cc8aa36d9e60a7e0e520a6e42cdc8dd
SantosRodrigo89 <Admin@CDC-NB04.CDC.TITAN>
2022-12-19 15:46:46 -0300

    feature: Link do Next está funcionando

--- pages/404.js
+++ pages/404.js
@@ -3,7 +3,7 @@ import Link from "../src/components/Link";
 export default function PaginaDePerguntas() {
   return (
     <div>
-      <h1>Página de perguntas FAQ</h1>
+      <h1>Você se perdeu e caiu na página 404 :O</h1>
       <Link href={"/"}>Ir para a Home Page</Link>
     </div>
   );
