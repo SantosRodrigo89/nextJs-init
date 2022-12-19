7710f3402184c4c40d0df69725509ca55788570d
SantosRodrigo89 <Admin@CDC-NB04.CDC.TITAN>
2022-12-19 16:47:35 -0300

    feature/ link estilizado

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Link from "../src/components/Link";
+import LinkEstilizado from "../src/components/Link/linkEstilizado";
 
 export default function HomePage() {
   return (
     <div>
-      <h1>Alura Cases</h1>
+      <LinkEstilizado>Alura Cases</LinkEstilizado>
       <Link href="/faq">Ir para página do FAQ</Link>
     </div>
   );
