e6c16b3d1a3c8bffae748503e65690a02fe6832b
rodrigovieiraminu <rodrigo.vieira@minu.co>
2024-02-29 17:12:13 -0300

    feat: auth 0

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -3,6 +3,7 @@ import Footer from "../../components/patterns/Footer";
 import Link from "../../components/Link";
 import { theme } from "../../theme/theme";
 import { Image, Box, Text, Icon, Input, Button } from "../../theme/components";
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 const LOGO_ALURA_URL = "/images/logo.png";
 const SIDE_IMAGE_URL = "/images/photo.jpg";
@@ -95,236 +96,246 @@ function SideImage() {
 }
 
 export default function HomeScreen() {
-  return (
-    <Box>
-      <Head>
-        <title>
-          {" "}
-          Prometech Labs - Seu Parceiro em Desenvolvimento de Sites!
-        </title>
-      </Head>
-      <Box
-        as="main"
-        styleSheet={{
-          flex: 1,
-          backgroundColor: theme.colors.neutral["050"],
-        }}
-      >
+
+  const { user, error, isLoading} = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  if (user) {
+    return (
+      <Box>
+        <Head>
+          <title>
+            {" "}
+            Prometech Labs - Seu Parceiro em Desenvolvimento de Sites!
+          </title>
+        </Head>
         <Box
+          as="main"
           styleSheet={{
-            overflow: "hidden",
-            position: {
-              lg: "relative",
-            },
-            paddingTop: {
-              xs: theme.space.x6,
-              sm: theme.space.x12,
-            },
-            paddingVertical: {
-              md: theme.space.x20,
-              lg: theme.space.x24,
-            },
+            flex: 1,
+            backgroundColor: theme.colors.neutral["050"],
           }}
         >
           <Box
             styleSheet={{
-              marginHorizontal: "auto",
-              paddingHorizontal: {
-                xs: theme.space.x4,
-                sm: theme.space.x6,
-                lg: theme.space.x8,
+              overflow: "hidden",
+              position: {
+                lg: "relative",
               },
-              maxWidth: {
-                sm: theme.space.xcontainer_md,
-                lg: theme.space.xcontainer_lg,
+              paddingTop: {
+                xs: theme.space.x6,
+                sm: theme.space.x12,
               },
-              display: {
-                lg: "grid",
-              },
-              gap: {
+              paddingVertical: {
+                md: theme.space.x20,
                 lg: theme.space.x24,
-              },
-              gridTemplateColumns: {
-                lg: "repeat(2, minmax(0, 1fr))",
               },
             }}
           >
-            <Box>
+            <Box
+              styleSheet={{
+                marginHorizontal: "auto",
+                paddingHorizontal: {
+                  xs: theme.space.x4,
+                  sm: theme.space.x6,
+                  lg: theme.space.x8,
+                },
+                maxWidth: {
+                  sm: theme.space.xcontainer_md,
+                  lg: theme.space.xcontainer_lg,
+                },
+                display: {
+                  lg: "grid",
+                },
+                gap: {
+                  lg: theme.space.x24,
+                },
+                gridTemplateColumns: {
+                  lg: "repeat(2, minmax(0, 1fr))",
+                },
+              }}
+            >
               <Box>
-                <Image
-                  styleSheet={{
-                    width: "auto",
-                    height: theme.space.x32,
-                  }}
-                  src={LOGO_ALURA_URL}
-                  alt="Logo Prometech Labs"
-                />
-              </Box>
-              <Box
-                styleSheet={{
-                  marginTop: {
-                    xs: theme.space.x16,
-                    sm: theme.space.x20,
-                  },
-                }}
-              >
                 <Box>
-                  <Link
-                    href="/faq"
+                  <Image
                     styleSheet={{
-                      display: "inline-flex",
-                      alignItems: {
-                        xs: "flex-start",
-                        sm: "center",
-                      },
-                      flexDirection: {
-                        xs: "column",
-                        sm: "row",
-                      },
+                      width: "auto",
+                      height: theme.space.x32,
                     }}
-                  >
-                    <Text
+                    src={LOGO_ALURA_URL}
+                    alt="Logo Prometech Labs"
+                  />
+                </Box>
+                <Box
+                  styleSheet={{
+                    marginTop: {
+                      xs: theme.space.x16,
+                      sm: theme.space.x20,
+                    },
+                  }}
+                >
+                  <Box>
+                    <Link
+                      href="/faq"
                       styleSheet={{
-                        textVariant: theme.typography.variants.body4,
-                        fontWeight: "600",
-                        borderRadius: theme.space.x64,
-                        color: theme.colors.primary["400"],
-                        backgroundColor: theme.colors.primary["100"],
-                        paddingHorizontal: theme.space["x2.5"],
-                        paddingVertical: theme.space["x1"],
-                        marginRight: theme.space.x2,
-                        marginBottom: {
-                          xs: theme.space.x2,
-                          sm: theme.space.x0,
+                        display: "inline-flex",
+                        alignItems: {
+                          xs: "flex-start",
+                          sm: "center",
+                        },
+                        flexDirection: {
+                          xs: "column",
+                          sm: "row",
                         },
                       }}
                     >
-                      O que tem de novo?
-                    </Text>
-                    <Text
-                      styleSheet={{
-                        textVariant: theme.typography.variants.body4,
-                        fontWeight: "600",
-                        display: "inline-flex",
-                        borderRadius: theme.space.x64,
-                        color: theme.colors.primary["400"],
-                        alignItems: "center",
-                      }}
-                    >
-                      <Text>Confira as principais dúvidas</Text>
-                      <Icon
+                      <Text
                         styleSheet={{
-                          iconVariant: "chevronRight",
-                          marginLeft: theme.space.xpx,
+                          textVariant: theme.typography.variants.body4,
+                          fontWeight: "600",
+                          borderRadius: theme.space.x64,
+                          color: theme.colors.primary["400"],
+                          backgroundColor: theme.colors.primary["100"],
+                          paddingHorizontal: theme.space["x2.5"],
+                          paddingVertical: theme.space["x1"],
+                          marginRight: theme.space.x2,
+                          marginBottom: {
+                            xs: theme.space.x2,
+                            sm: theme.space.x0,
+                          },
                         }}
-                        aria-hidden="true"
-                      />
-                    </Text>
-                  </Link>
-                </Box>
-                <Box
-                  styleSheet={{
-                    marginTop: theme.space.x6,
-                    maxWidth: theme.space.xcontainer_sm,
-                  }}
-                >
-                  <Text
-                    as="h1"
-                    styleSheet={{
-                      textVariant: theme.typography.variants.heading1,
-                      color: theme.colors.neutral["900"],
-                    }}
-                  >
-                    Prometech Labs
-                  </Text>
-                  <Text
-                    as="p"
-                    styleSheet={{
-                      textVariant: theme.typography.variants.body1,
-                      color: theme.colors.neutral["500"],
-                      marginTop: theme.space.x6,
-                    }}
-                  >
-                    Bem-vindo à Prometech Labs - Seu Parceiro em Desenvolvimento
-                    de Sites! Somos especializados em React e Node.js, criando
-                    sites atraentes e eficientes. Oferecemos soluções
-                    personalizadas, garantindo desempenho e suporte contínuo.
-                  </Text>
-                  <Text
-                    as="p"
-                    styleSheet={{
-                      textVariant: theme.typography.variants.body1,
-                      color: theme.colors.neutral["500"],
-                      marginTop: theme.space.x6,
-                    }}
-                  >
-                    Contate-nos para transformar sua visão digital em realidade!
-                  </Text>
-                </Box>
-                <Box
-                  as="form"
-                  action="#"
-                  styleSheet={{
-                    display: {
-                      sm: "flex",
-                    },
-                    marginTop: theme.space.x12,
-                    width: {
-                      sm: theme.space["x1/1"],
-                    },
-                    maxWidth: {
-                      sm: theme.space.xcontainer_lg,
-                    },
-                  }}
-                >
-                  <Box
-                    styleSheet={{
-                      minWidth: 0,
-                      flex: 1,
-                    }}
-                  >
-                    <Text
-                      as="label"
-                      htmlFor="email"
-                      styleSheet={{
-                        srOnly: true,
-                      }}
-                    >
-                      Email address
-                    </Text>
-                    <Input
-                      id="email"
-                      type="email"
-                      placeholder="Coloque seu email aqui"
-                    />
+                      >
+                        O que tem de novo?
+                      </Text>
+                      <Text
+                        styleSheet={{
+                          textVariant: theme.typography.variants.body4,
+                          fontWeight: "600",
+                          display: "inline-flex",
+                          borderRadius: theme.space.x64,
+                          color: theme.colors.primary["400"],
+                          alignItems: "center",
+                        }}
+                      >
+                        <Text>Confira as principais dúvidas</Text>
+                        <Icon
+                          styleSheet={{
+                            iconVariant: "chevronRight",
+                            marginLeft: theme.space.xpx,
+                          }}
+                          aria-hidden="true"
+                        />
+                      </Text>
+                    </Link>
                   </Box>
                   <Box
                     styleSheet={{
-                      marginTop: {
-                        xs: theme.space.x4,
-                        sm: theme.space.x0,
+                      marginTop: theme.space.x6,
+                      maxWidth: theme.space.xcontainer_sm,
+                    }}
+                  >
+                    <Text
+                      as="h1"
+                      styleSheet={{
+                        textVariant: theme.typography.variants.heading1,
+                        color: theme.colors.neutral["900"],
+                      }}
+                    >
+                      Prometech Labs
+                    </Text>
+                    <Text
+                      as="p"
+                      styleSheet={{
+                        textVariant: theme.typography.variants.body1,
+                        color: theme.colors.neutral["500"],
+                        marginTop: theme.space.x6,
+                      }}
+                    >
+                      Bem-vindo à Prometech Labs - Seu Parceiro em Desenvolvimento
+                      de Sites! Somos especializados em React e Node.js, criando
+                      sites atraentes e eficientes. Oferecemos soluções
+                      personalizadas, garantindo desempenho e suporte contínuo.
+                    </Text>
+                    <Text
+                      as="p"
+                      styleSheet={{
+                        textVariant: theme.typography.variants.body1,
+                        color: theme.colors.neutral["500"],
+                        marginTop: theme.space.x6,
+                      }}
+                    >
+                      Contate-nos para transformar sua visão digital em realidade!
+                    </Text>
+                  </Box>
+                  <Box
+                    as="form"
+                    action="#"
+                    styleSheet={{
+                      display: {
+                        sm: "flex",
                       },
-                      marginLeft: {
-                        sm: theme.space.x3,
+                      marginTop: theme.space.x12,
+                      width: {
+                        sm: theme.space["x1/1"],
+                      },
+                      maxWidth: {
+                        sm: theme.space.xcontainer_lg,
                       },
                     }}
                   >
-                    <Button
-                      type="submit"
-                      // button variant
+                    <Box
+                      styleSheet={{
+                        minWidth: 0,
+                        flex: 1,
+                      }}
                     >
-                      Cadastrar
-                    </Button>
+                      <Text
+                        as="label"
+                        htmlFor="email"
+                        styleSheet={{
+                          srOnly: true,
+                        }}
+                      >
+                        Email address
+                      </Text>
+                      <Input
+                        id="email"
+                        type="email"
+                        placeholder="Coloque seu email aqui"
+                      />
+                    </Box>
+                    <Box
+                      styleSheet={{
+                        marginTop: {
+                          xs: theme.space.x4,
+                          sm: theme.space.x0,
+                        },
+                        marginLeft: {
+                          sm: theme.space.x3,
+                        },
+                      }}
+                    >
+                      <Button
+                        type="submit"
+                        // button variant
+                      >
+                        Cadastrar
+                      </Button>
+                    </Box>
                   </Box>
                 </Box>
               </Box>
             </Box>
+  
+            <SideImage />
           </Box>
-
-          <SideImage />
         </Box>
+        <Footer />
       </Box>
-      <Footer />
-    </Box>
-  );
+    );
+  }
+
+  return <a href="/api/auth/login">Login</a>;
 }
